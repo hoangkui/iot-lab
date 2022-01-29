e59d3ee86f4859244fe4ae5ba0e5e15a5fe43e0b
hoangkui <hoangkui2001@gmail.com>
2022-01-29 16:50:04 +0700

    update function get longitude and latitude

--- Lab1/main.py
+++ Lab1/main.py
@@ -2,7 +2,9 @@ print("Xin chào ThingsBoard")
 import paho.mqtt.client as mqttclient
 import time
 import json
-import geocoder
+import subprocess as sp
+import re
+
 
 BROKER_ADDRESS = "demo.thingsboard.io"
 PORT = 1883
@@ -32,6 +34,32 @@ def connected(client, usedata, flags, rc):
     else:
         print("Connection is failed")
 
+# Get longitude latitude 
+def getLocation():
+    accuracy=3
+    pshellcomm=["powershell"]
+    pshellcomm.append('add-type -assemblyname system.device; ' \
+        '$loc = new-object system.device.location.geocoordinatewatcher;' \
+        '$loc.start(); ' \
+        'while(($loc.status -ne "Ready") -and ($loc.permission -ne "Denied")) ' \
+        '{start-sleep -milliseconds 100}; ' \
+        '$acc = %d; ' \
+        'while($loc.position.location.horizontalaccuracy -gt $acc) ' \
+        '{start-sleep -milliseconds 100; $acc = [math]::Round($acc*1.5)}; ' \
+        '$loc.position.location.latitude; ' \
+        '$loc.position.location.longitude; ' \
+        '$loc.position.location.horizontalaccuracy; ' \
+        '$loc.stop()' % (accuracy))
+    p = sp.Popen(pshellcomm, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.STDOUT, text=True)
+    (out, err) = p.communicate()
+    out = re.split('\n', out)
+    longitude = float(out[1])
+    latitude = float(out[0])
+    # print(out)
+    return longitude,latitude
+
+
+
 
 client = mqttclient.Client("Gateway_Thingsboard")
 client.username_pw_set(THINGS_BOARD_ACCESS_TOKEN)
@@ -49,17 +77,15 @@ light_intesity = 100
 counter = 0
 
 
-# print(g.latlng)
-longitude=108.0378
-latitude=12.6675
+
+longitude=106.6297
+latitude=10.8231
 while True:
     collect_data = {'temperature': temp, 'humidity': humi, 'light':light_intesity,'longitude':longitude,'latitude':latitude}
     temp += 1
     humi += 1
-    # g = geocoder.ip('me')
-    # print(g.latlng)
-    # latitude=g.latlng[0]
-    # longitude=g.latlng[1]
+    longitude,latitude=getLocation()
     light_intesity += 1
     client.publish('v1/devices/me/telemetry', json.dumps(collect_data), 1)
+    print(collect_data)
     time.sleep(5)
